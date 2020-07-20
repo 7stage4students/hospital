4eeec721fca1a85e9fa925ca04e086086949abfc
sedoid <karlsedoide@gmail.com>
2020-07-20 12:15:31 -0400

    fix: improved the backend

--- index.js
+++ index.js
@@ -1,22 +1,42 @@
+
 require('dotenv').config()
 
 const express = require('express');
-const app = express();
+const path = require('path')
+const adminRouter = require('./routes/admin')
+const userRouter = require('./routes/user');
+const cookieParser= require('cookie-parser');
+const userTracker = require('./utils/userTracker')
+const exphbs = require("express-handlebars");
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.DATABASE_URL, { useUnifiedTopology: true, useNewUrlParser: true }, () => {
-    console.log('Database Connected Successfully')
+const app = express();
+
+mongoose.connect(process.env.DATABASE_URL, { useUnifiedTopology: true, useNewUrlParser: true }, (err,res) => { 
+   console.log('Connected to Database');
 });
 
-// cofigure our server
+
+app.use(cookieParser())
+app.use('/',userTracker)
+
+app.engine("handlebars", exphbs({partialsDir: path.join(__dirname + '/views/partials') } ));
+app.set("view engine", "handlebars");
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
-app.get('/', (req,res)=>{
-    res.send('ok ')
+
+app.get('/',(req,res)=>{
+     res.render('home')
+})
+app.get('/login',(req,res)=>{
+    res.render('login')
 })
 
-
+app.use('/admin', adminRouter);
+app.use('/user', userRouter)
 
 
 app.listen(process.env.PORT, () => {
     console.log("server started");
 });
+
---
+++ routes/admin.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const auth = require('../controlers/auth');
+const formidable = require('formidable');
+const {getAll,getById} = require('../controlers/admin');
+
+
+const form = formidable({ multiples: true });
+const router = express.Router();
+
+
+router.use(express.json())
+
+   router.get('/login', (req,res)=>{
+         res.render('adminlogin'); 
+   });
+
+   router.post('/login', (req,res)=>{
+
+    form.parse(req, (err, fields, files) => {
+        if (err) {
+            next(err);
+            return;
+        }
+        else{
+            console.log(fields);
+            auth.postAdminLogin(fields,res,null);
+        }
+    });
+    //    res.render('addmin')
+
+   });
+
+   router.get('/home',(req, res)=>{
+       if(req.user){
+           res.render('adminpage')
+       }else
+           res.render('adminlogin');
+   })
+
+   router.get('/patients/all',(req, res) =>{
+        console.log('Getting all ');
+        console.log(req.user)
+        getAll(res);
+ 
+   })
+
+   router.get('/patient/:id',( req, res )=>{
+       
+       let id = req.params.id;
+       let sample = 'YQXWzHavQAMl6U1YjOURvlv9u3O0N9xR7KQ5F9ekPj0=';
+    //    if (req.user){
+            getById(sample,res)
+    //    }else
+            // res.render('adminlogin');      
+   });
+
+   router.post('/add',( req, res )=>{
+        if (req.user){
+            res.send('Adding single patient')
+        }else
+            res.render('adminlogin');
+        
+   });
+
+    router.patch('/update/:id',( req, res )=>{
+        if (req.user){
+            res.render('Updating a user')
+        }else
+            res.redirect('/admin/login');
+       
+    });
+
+
+   module.exports = router
