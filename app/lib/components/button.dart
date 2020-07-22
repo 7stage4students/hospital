0dd611679332396772254127edf071b08b49588f
Laslie Georges JR <laslie.georgesjr@gmail.com>
2020-07-22 14:18:17 +0100

    update: fixed manage screen

--- app/lib/components/button.dart
+++ app/lib/components/button.dart
@@ -2,46 +2,44 @@ import 'package:flutter/material.dart';
 import 'package:seven_health/style/style.dart';
 
 class StandardButton extends StatelessWidget {
-final String btnText;
-final Color btnColor;
-final VoidCallback onPress;
+  final String btnText;
+  final Color btnColor;
+  final VoidCallback onPress;
 
-
-  const StandardButton({Key key, this.btnText, this.btnColor,  this.onPress}) : super(key: key);
-
-  
+  const StandardButton({Key key, this.btnText, this.btnColor, this.onPress})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-  return  RaisedButton(
-    elevation: 0,
-    highlightElevation: 0,
-    textColor: Colors.white,
-    padding: const EdgeInsets.all(0.0),
-    shape: RoundedRectangleBorder(
-      borderRadius: BorderRadius.circular(8.0),
-    ),
-    onPressed: onPress,
-    child: Container(
-      width: screenWidth(context) / 1.2,
-      height: 60,
-      decoration: BoxDecoration(
-        color: this.btnColor,
-        borderRadius: BorderRadius.all(
-          Radius.circular(8.0),
+    return MaterialButton(
+      elevation: 0,
+      highlightElevation: 0,
+      textColor: Colors.white,
+      padding: const EdgeInsets.all(0.0),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(8.0),
+      ),
+      onPressed: onPress,
+      child: Container(
+        width: screenWidth(context) / 1.2,
+        height: 60,
+        decoration: BoxDecoration(
+          color: this.btnColor,
+          borderRadius: BorderRadius.all(
+            Radius.circular(8.0),
+          ),
+        ),
+        padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              this.btnText,
+              style: btnTextStyle(),
+            ),
+          ],
         ),
       ),
-      padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Text(
-            this.btnText,
-            style: btnTextStyle(),
-          ),
-        ],
-      ),
-    ),
-  );
+    );
   }
 }
--- app/lib/screens/manage/manage_screen.dart
+++ app/lib/screens/manage/manage_screen.dart
@@ -1,10 +1,11 @@
-
 import 'package:flutter/material.dart';
 import 'package:giffy_dialog/giffy_dialog.dart';
 import 'package:seven_health/components/app_bar.dart';
 import 'package:seven_health/components/button.dart';
 import 'package:seven_health/screens/not_found/not_found_screen.dart';
 import 'package:seven_health/style/style.dart';
+import 'package:seven_health/utils/utils.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class ManageScreen extends StatefulWidget {
   final dynamic data;
@@ -23,72 +24,97 @@ class _ManageScreenState extends State<ManageScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: getAppBar("Manage Patient", showBackArrow:  false),
-        backgroundColor: Colors.white,
-        body:  new WillPopScope(onWillPop: ()async=>false,
-        child: widget.data["usesssrId"] == null ? buildNotFound(context):buildStudentInfo()), );
+      appBar: getAppBar("Manage Patient", showBackArrow: false),
+      backgroundColor: Colors.white,
+      body: new WillPopScope(
+          onWillPop: () async => false,
+          child: widget.data["userId"] == null
+              ? buildNotFound(context)
+              : buildStudentInfo()),
+    );
   }
 
-  Column buildStudentInfo() {
+  buildStudentInfo() {
     print(widget.data);
-    return Column(
-        children: <Widget>[
-          ///student picture
-          ClipRect(
-            child: Image.asset(
-              "assets/img/freddy.jpg",
-              fit: BoxFit.cover,
-            ),
+    return ListView(
+      children: <Widget>[
+        ///student picture
+        ClipRect(
+          child: Image.asset(
+            "assets/img/freddy.jpg",
+            fit: BoxFit.cover,
           ),
-          SizedBox(
-            height: 20,
-          ),
-          Text(
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        Center(
+          child: Text(
             "Freddy Tamwo",
-            style: titleStyle().copyWith(color: Colors.grey, fontSize: 32),
+            style: titleStyle().copyWith(color: Colors.black, fontSize: 32),
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: <Widget>[
               Text(
-                "Web Development" + " - ",
+                "Blood Group" + " A+ ",
+                style: TextStyle(color: Colors.blue,fontWeight: FontWeight.bold),
               ),
               SizedBox(
                 width: 8,
               ),
               Text(
-               "test",
-                style: TextStyle(fontWeight: FontWeight.w400),
+                "26 yrs old",
+                style: TextStyle(fontWeight: FontWeight.w800,color: Colors.blue),
+              ),
+              MaterialButton(onPressed: ()=>launchInBrowser("https://www.google.com"),
+              child: Row(
+                children: <Widget>[
+                  Icon(Icons.phone,color: Colors.red,),
+                  SizedBox(width: 8,),
+                  Text("Emergency",style: TextStyle(color: Colors.red),)
+                ],
+              ),
               )
             ],
           ),
-          SizedBox(height: 16,),
+        ),
+        SizedBox(
+          height: 16,
+        ),
 
-          ///Mark Presetn
-          StandardButton(
-            btnText: "Manage Patient",
-            btnColor: tertiary,
-            onPress: _showUserInfo,
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          StandardButton(
-            btnText: "Search New Patient",
-            btnColor: primary,
-            onPress:()=> Navigator.of(context).popUntil(ModalRoute.withName("/")),
-            
-          ),
-          SizedBox(
-            height: 20,
-          ),
-         
+        ///Mark Presetn
+        StandardButton(
+          btnText: "Manage Patient",
+          btnColor: tertiary,
+          onPress: _showUserInfo,
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        StandardButton(
+          btnText: "View History",
+          btnColor: primary,
+          onPress: _showUserInfo,
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        StandardButton(
+          btnText: "Search New Patient",
+          btnColor: primary,
+          onPress: () =>
+              Navigator.of(context).popUntil(ModalRoute.withName("/")),
+        ),
 
-          ///Manage Student
+        ///Manage Student
 
-          ///disable account
-        ],
-      );
+        ///disable account
+      ],
+    );
   }
 
   Future<Null> _showUserInfo() {
@@ -96,20 +122,20 @@ class _ManageScreenState extends State<ManageScreen> {
       context: context,
       builder: (_) => NetworkGiffyDialog(
         image: Image.asset(
-          "assets/img/freddy.jpg",
+          "assets/img/login.png",
           fit: BoxFit.cover,
         ),
-        title: Text('Tamwo Nechie Freddy',
+        title: Text('Login To Continue',
             textAlign: TextAlign.center,
             style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.w600)),
         description: Text(
-          'Do you want to mark name as present ?',
+          'Only approved doctors can view patient history.',
           textAlign: TextAlign.center,
         ),
         entryAnimation: EntryAnimation.DEFAULT,
-        onOkButtonPressed: () {},
+        onOkButtonPressed: () => launchInBrowser("https://www.google.com"),
         buttonOkText: Text(
-          'Yes',
+          'Login',
           style: TextStyle(
             color: Colors.white,
           ),
