0d279f1f4d6f7a7dc80a2446767216d48987050b
Laslie Georges JR <laslie.georgesjr@gmail.com>
2020-07-24 16:13:27 +0100

    routes and views update

--- app/lib/screens/manage/manage_screen.dart
+++ app/lib/screens/manage/manage_screen.dart
@@ -1,10 +1,10 @@
-
 import 'package:flutter/material.dart';
 import 'package:giffy_dialog/giffy_dialog.dart';
 import 'package:seven_health/components/app_bar.dart';
 import 'package:seven_health/components/button.dart';
 import 'package:seven_health/screens/not_found/not_found_screen.dart';
 import 'package:seven_health/style/style.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class ManageScreen extends StatefulWidget {
   final dynamic data;
@@ -23,72 +23,98 @@ class _ManageScreenState extends State<ManageScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: getAppBar("Manage Patient", showBackArrow:  false),
-        backgroundColor: Colors.white,
-        body:  new WillPopScope(onWillPop: ()async=>false,
-        child: widget.data["usesssrId"] == null ? buildNotFound(context):buildStudentInfo()), );
+      appBar: getAppBar("Manage Patient", showBackArrow: false),
+      backgroundColor: Colors.white,
+      body: new WillPopScope(
+          onWillPop: () async => false,
+          child: widget.data["userId"] == null
+              ? buildNotFound(context)
+              : buildStudentInfo()),
+    );
   }
 
   Column buildStudentInfo() {
     print(widget.data);
     return Column(
-        children: <Widget>[
-          ///student picture
-          ClipRect(
-            child: Image.asset(
-              "assets/img/freddy.jpg",
-              fit: BoxFit.cover,
-            ),
+      children: <Widget>[
+        ///student picture
+        ClipRect(
+          child: Image.asset(
+            "assets/img/freddy.jpg",
+            fit: BoxFit.cover,
           ),
-          SizedBox(
-            height: 20,
-          ),
-          Text(
-            "Freddy Tamwo",
-            style: titleStyle().copyWith(color: Colors.grey, fontSize: 32),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        Text(
+          "Freddy Tamwo",
+          style: titleStyle().copyWith(color: Colors.grey, fontSize: 32),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: <Widget>[
               Text(
-                "Web Development" + " - ",
+                "Blood Group" + " A+ ",
               ),
               SizedBox(
                 width: 8,
               ),
               Text(
-               "test",
+                "23 yrs",
                 style: TextStyle(fontWeight: FontWeight.w400),
+              ),
+              MaterialButton(
+                onPressed: () => {},
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.phone,
+                      color: Colors.red,
+                    ),
+                    SizedBox(
+                      width: 5.0,
+                    ),
+                    Text(
+                      "Emergency Contact",
+                      style: TextStyle(color: Colors.red),
+                    )
+                  ],
+                ),
               )
             ],
           ),
-          SizedBox(height: 16,),
+        ),
+        SizedBox(
+          height: 16,
+        ),
 
-          ///Mark Presetn
-          StandardButton(
-            btnText: "Manage Patient",
-            btnColor: tertiary,
-            onPress: _showUserInfo,
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          StandardButton(
-            btnText: "Search New Patient",
-            btnColor: primary,
-            onPress:()=> Navigator.of(context).popUntil(ModalRoute.withName("/")),
-            
-          ),
-          SizedBox(
-            height: 20,
-          ),
-         
+        ///Mark
+        StandardButton(
+          btnText: "Manage Patient",
+          btnColor: tertiary,
+          onPress: _showUserInfo,
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        StandardButton(
+          btnText: "Search New Patient",
+          btnColor: primary,
+          onPress: () =>
+              Navigator.of(context).popUntil(ModalRoute.withName("/")),
+        ),
+        SizedBox(
+          height: 20,
+        ),
 
-          ///Manage Student
+        ///Manage Student
 
-          ///disable account
-        ],
-      );
+        ///disable account
+      ],
+    );
   }
 
   Future<Null> _showUserInfo() {
@@ -99,17 +125,17 @@ class _ManageScreenState extends State<ManageScreen> {
           "assets/img/freddy.jpg",
           fit: BoxFit.cover,
         ),
-        title: Text('Tamwo Nechie Freddy',
+        title: Text('Manage Patient',
             textAlign: TextAlign.center,
             style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.w600)),
         description: Text(
-          'Do you want to mark name as present ?',
+          'You must be an admin to view the patient.',
           textAlign: TextAlign.center,
         ),
         entryAnimation: EntryAnimation.DEFAULT,
-        onOkButtonPressed: () {},
+        onOkButtonPressed: () => _launchURL(),
         buttonOkText: Text(
-          'Yes',
+          'Login',
           style: TextStyle(
             color: Colors.white,
           ),
@@ -117,5 +143,14 @@ class _ManageScreenState extends State<ManageScreen> {
         buttonOkColor: primary,
       ),
     );
+  }
+
+  _launchURL() async {
+    const url = 'https://hospital-966e7.web.app/login';
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
   }
 }
